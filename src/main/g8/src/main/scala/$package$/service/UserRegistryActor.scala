52496a65c2da07efbff88bada4575781a9d280c2
Avi Levi <123avi@gmail.com>
2019-08-09 15:54:14 +0300

    another try using esc

--- src/main/g8/src/main/scala/$package$/service/UserRegistryActor.scala
+++ src/main/g8/src/main/scala/$package$/service/UserRegistryActor.scala
@@ -28,13 +28,13 @@ class UserRegistryActor extends Actor with ActorLogging {
       sender() ! Users(users.toSeq)
     case CreateUser(user) =>
       users += user
-      sender() ! ActionPerformed(s"User \${user.name} created.")
+      sender() ! ActionPerformed(s"User "+user.name"+" created.")
 
     case GetUser(name) =>
       sender() ! users.find(_.name == name)
     case DeleteUser(name) =>
       users.find(_.name == name) foreach { user => users -= user }
-      sender() ! ActionPerformed("User"+name+" deleted.")
+      sender() ! ActionPerformed("User "+name+" deleted.")
   }
 }
 //#user-registry-actor
